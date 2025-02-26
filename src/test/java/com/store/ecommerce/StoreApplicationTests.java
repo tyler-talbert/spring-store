4e04ef0ae02df210b5414746eb97a472952ba4a7
anonymous-cloud-192 <Sparklecleansoutheast@gmail.com>
2025-02-26 11:33:51 -0500

    Initial commit

---
+++ src/test/java/com/store/ecommerce/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
